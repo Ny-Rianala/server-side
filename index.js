df6a5f095d30b7f82b0b152b3e7562c2f1f6fbdc
kati <fanomezantsoakatiuscia048@gmail.com>
2020-08-25 11:06:20 +0300

    added port

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Greetings from Node and index.js!');
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app is running here:http://local:${port}`)
+});
+
+app.get('/', (request, response) => {
+  console.log('Request: /');
+  response.sendFile(__dirname + '/index.html');
+});
